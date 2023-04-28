e8be2a829ba62e7caad8bb6e929c457800fb3851
Kevin C-Dubois <kcdubois@paloaltonetworks.com>
2023-04-28 14:14:14 -0400

    added policies

--- examples/terraform/efs.tf
+++ examples/terraform/efs.tf
@@ -25,6 +25,23 @@ data "aws_iam_policy_document" "policy" {
       values   = ["true"]
     }
   }
+  statement {
+    sid    = "ExampleStatement02"
+    effect = "Allow"
+
+    actions = [
+      "elasticfilesystem:ClientMount",
+      "elasticfilesystem:ClientWrite",
+    ]
+
+    resources = [aws_efs_file_system.fs.arn]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["true"]
+    }
+  }
 }
 
 resource "aws_efs_file_system_policy" "policy" {
